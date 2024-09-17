4d193567256d3efc9a48c5f7530fc598b68bb141
Jonathan Hachette <jonathan.hachette@gmail.com>
2024-09-17 14:20:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from '../src/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <main class="container py-8">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="container py-8">
+    <header class="flex gap-4">
+      <RouterLink :to="{ name: 'home' }"> <Button>Home </Button> </RouterLink>
+      <Button @click="toggleDark()" variant="default">Dark mode </Button>
+      <button
+        class="bg-secondary text-primary-foreground border border-border px-4 py-2 rounded-lg"
+      >
+        Mon bouton
+      </button>
+    </header>
+  </div>
+</template>
+
+<style scoped></style>
